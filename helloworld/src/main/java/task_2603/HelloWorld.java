a5ebbe4ad1db801a161e985d80af7a69d88caaa4
jon <jon@litterqer.com>
2022-07-04 14:05:36 +1000

    test branch commit

--- helloworld/src/main/java/task_2603/HelloWorld.java
+++ helloworld/src/main/java/task_2603/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // 这行注释仅用来测试git commit
+        // 这行注释仅用来测试提交到test1分支上
     }
 }
