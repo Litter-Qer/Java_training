09c0527a076c611a144247c8f6889a634ec56c71
jon <jon@litterqer.com>
2022-07-04 14:27:39 +1000

    update HelloWord

--- helloworld/src/main/java/task_2603/HelloWorld.java
+++ helloworld/src/main/java/task_2603/HelloWorld.java
@@ -3,6 +3,7 @@ package task_2603;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("此行用于制造git冲突");
         // 这行注释仅用来测试git commit
         // 这行注释仅用来测试提交到test2分支上
     }
