a2a5814699eb051c693c66aa9451d2b498e1284b
jon <jon@litterqer.com>
2022-08-16 14:59:17 +1000

    线程进阶7

---
+++ TASK_2618/src/main/java/OfficeTest.java
@@ -0,0 +1,51 @@
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+@Slf4j
+public class OfficeTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        test1();
+    }
+
+    public static void test1() throws InterruptedException {
+        Office office = new Office();
+        new Thread(() -> {
+            try {
+                office.operating();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"OP-Thread").start();
+
+        new Thread(() -> {
+            try {
+                office.coding();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"IT-Thread").start();
+    }
+}
+
+@Slf4j
+class Office {
+    private final Object OPTable = new Object();
+    private final Object ITTable = new Object();
+
+    public void operating() throws InterruptedException {
+        synchronized (OPTable) {
+            log.debug("运维工作一会儿");
+            TimeUnit.SECONDS.sleep(2);
+        }
+    }
+
+    public void coding() throws InterruptedException {
+        synchronized (ITTable) {
+            log.debug("IT工作一会儿");
+            TimeUnit.SECONDS.sleep(2);
+        }
+    }
+}
